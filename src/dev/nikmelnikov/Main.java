b717ca1c2e0912a50d609f686c97e88485660c44
Nikita Melnikov <sc17nm@leeds.ac.uk>
2019-04-15 17:53:54 +0100

    Initialising repository

---
+++ src/dev/nikmelnikov/Main.java
@@ -0,0 +1,8 @@
+package dev.nikmelnikov;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // test
+    }
+}
---
+++ src/dev/nikmelnikov/blackjack/Blackjack.java
@@ -0,0 +1,20 @@
+package dev.nikmelnikov.blackjack;
+
+import java.util.ArrayList;
+
+/**
+ * Class containing the rules and functionality for the simple blackjack
+ * casino game.
+ */
+public class Blackjack {
+
+    // Array that stores the information of the available cards in the deck
+    private static ArrayList<String> deck = new ArrayList<>();
+    // AH, 2H, 3H, 4H, 5H, 6H, 7H, 8H, 9H, JH, QH, KH
+    // AS, 2S, 3S, 4S, 5S, 6S, 7S, 8S, 9S, JS, QS, KS
+
+
+    public static void main(String[] args) {
+
+    }
+}
